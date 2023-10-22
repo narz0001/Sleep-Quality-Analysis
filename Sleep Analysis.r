cf32985424287a8a39090803a2a6655ae7e116e7
Geolangsat Narzary <142396058+narz0001@users.noreply.github.com>
2023-10-23 06:07:58 +0800

    Preliminary Data Exploration and Minimal Data Manipulation

--- Sleep Analysis.r
+++ Sleep Analysis.r
@@ -18,3 +18,36 @@ summary(df)
 
 #Backup of Original df
 original_df = df
+
+####################### Preliminary Data Exploration ##########################
+
+#Checking number of NA values in the df
+na_count <- colSums(is.na(df))
+print("Number of NAs\n")
+print(na_count)
+#0 NA values
+
+#--------------------------------------------------------------------------------------------------------
+
+#Finding duplicate values in Person.ID
+duplicates <- df[duplicated(df$Person.ID) | duplicated(df$Person.ID, fromLast = TRUE), ]
+print(duplicates)
+#0 NA values
+
+#--------------------------------------------------------------------------------------------------------
+
+#Converting character variables to factor type for easy data manipulation of categorical variables
+for (i in 1:ncol(df)) {
+  if (is.character(df[, i])) {  # Added a closing parenthesis here
+    df[, i] = as.factor(df[, i])
+  }
+}
+
+#--------------------------------------------------------------------------------------------------------
+
+#Moving Target Variable - Quality of Sleep to the end of the DataFrame
+qos_index <- which(names(df) == "Quality.of.Sleep")
+col_names <- names(df)
+df <- df[, c(col_names[-qos_index], "Quality.of.Sleep")]
+
+#--------------------------------------------------------------------------------------------------------
